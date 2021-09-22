5415d0126f6ad23940a83dc5f2f2d139b3599272
Andy Oertel <andy.oertel@s2015.tu-chemnitz.de>
2021-09-22 19:36:43 +0200

    Add complete test/comparison for the Jacobi method

--- benches/jacobi_test.rs
+++ benches/jacobi_test.rs
@@ -1,8 +1,7 @@
 mod utils;
 
 use criterion::{criterion_group, criterion_main, Criterion};
-use nalgebra::{DMatrix, DVector};
-use std::io;
+use nalgebra::DVector;
 use std::time::SystemTime;
 use ultrametric_multiplication::RootedTreeVertex;
 
@@ -10,24 +9,52 @@ criterion_group!(benches, benchmark_jacobi);
 criterion_main!(benches);
 
 fn benchmark_jacobi(_c: &mut Criterion) {
-    let size = 100;
-    let mut off_diag = utils::generate_random_ultrametric_matrix(size);
+    let size = 20000;
+    let diag_elem = (size * size) as f64;
+    let mut matrix = utils::generate_random_ultrametric_matrix(size);
+    let mut off_diag = matrix.clone();
     let mut diag = DVector::zeros(size);
     for i in 0..size {
-        diag[i] = off_diag[(i, i)];
+        diag[i] = diag_elem;
+        matrix[(i, i)] = diag_elem;
         off_diag[(i, i)] = 0.;
     } 
     let b = utils::generate_random_vector(size);
-    let mut x = utils::generate_random_vector(size);
-    let mut conv = 1.;
-    dbg!(conv);
+    let x_start = utils::generate_random_vector(size);
+    let mut x = x_start.clone();
+    let mut conv = ((&matrix * &x) - &b).norm();
+
+    let start_normal = SystemTime::now();
     while conv > 10e-4 {
         let sigma = &off_diag * &x;
         let diff = &b - sigma;
         for i in 0..size {
             x[i] = diff[i] / diag[i];
         }
-        let conv = diff.norm();
-        dbg!(conv);
+        conv = ((&matrix * &x) - &b).norm();
     }
+    let duration_normal = start_normal.elapsed().unwrap();
+
+    let mut x = x_start.clone();
+    conv = ((&matrix * &x) - &b).norm();
+
+    let start_tree_gen = SystemTime::now();
+    let mut tree = RootedTreeVertex::get_partition_tree(&off_diag);
+    let duration_tree_gen = start_tree_gen.elapsed().unwrap();
+
+    let start_fast = SystemTime::now();
+    while conv > 10e-4 {
+        let sigma = tree.multiply_with_tree(&x);
+        let diff = &b - sigma;
+        for i in 0..size {
+            x[i] = diff[i] / diag[i];
+        }
+        conv = ((&matrix * &x) - &b).norm();
+    }
+    let duration_fast = start_fast.elapsed().unwrap();
+
+    println!("Normal time: {:?}", duration_normal);
+    println!("Tree gen time: {:?}", duration_tree_gen);
+    println!("Fast Jacobi alone time: {:?}", duration_fast);
+    println!("Fast Jacobi complete time: {:?}", duration_tree_gen + duration_fast);
 }
